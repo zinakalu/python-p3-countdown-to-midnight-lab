2245977e501a5b53df68f1e4efe3f7d042a5233a
zinakalu <zina.kalu@gmail.com>
2023-04-03 15:04:00 -0400

    countdown to midnight

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,33 @@
 # your code goes here!
+import time
+import ipdb
+
+def countdown():
+    i = 10
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print('HAPPY NEW YEAR!')
+
+    #or
+#         if i == 0:
+#             print('HAPPY NEW YEAR!')
+#         else:
+#             print(f"{i} SECOND(S)")
+#         i -= 1
+# ipdb.set_trace()
+        # time.sleep(1)
+        # print("HAPPY NEW YEAR!")
+
+# def countdown_with_sleep(n):
+#     for i in range(n, -1, -1):
+#         print(i)
+#         time.sleep(1)
+
+def countdown_with_sleep(i):
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    time.sleep()
+    print('HAPPY NEW YEAR!')
+
